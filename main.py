3d27a22a28be91590b89ce0f034b071f1416ef27
Arnon Portal <arnon.portal@gmail.com>
2022-06-19 13:36:11 +0300

    Unified

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os.path
 import urllib.request
 import json
+from urllib.error import HTTPError
 # import pdfplumber
 
 api_key = '5f0c4de627a9354bdfcac23fbaacc3d2'
@@ -12,8 +13,7 @@ def dataset_to_list(path_to_dataset):
 
 
 
-def get_actors(person_id):
-    url = f'https://api.themoviedb.org/3/person/{person_id}?api_key={api_key}&language=en-US'
+def get_url_data(url):
     request = urllib.request.Request(url)
     result = urllib.request.urlopen(request)
     result_text = result.read()
@@ -36,32 +36,34 @@ def get_movie_char(actor_id):
     return ret_list
 
 
-def get_actors(max_id):
+def create_database(max_id):
     ret_list = []
-    for curr_id in range(1, max_id):
-        url = f"https://api.themoviedb.org/3/person/{curr_id}?api_key=5f0c4de627a9354bdfcac23fbaacc3d2&language=en-US"
-        try:
-            data = get_url_data(url)
-        except HTTPError as err:
-            if err.code == 404:
-                print(f"ERROR 404 for id {curr_id}, skipping")
-                continue
+    with open("./dataset.json", "w") as f:
+        for curr_id in range(1, max_id):
+            url = f"https://api.themoviedb.org/3/person/{curr_id}?api_key=5f0c4de627a9354bdfcac23fbaacc3d2&language=en-US"
+            try:
+                data = get_url_data(url)
+            except HTTPError as err:
+                if err.code != 200:
+                    print(f"Error {err} for id {curr_id}, skipping...")
+                    continue
 
-        # Check if place of birth exists -> if true, check if he's american
-        birth_place = data.get('place_of_birth')
-        places = ['United States', 'USA', 'U.S.']
-        is_american = any(place in birth_place for place in places) if birth_place else False
-        is_actor = data['known_for_department'] == "Acting"
+            # Check if place of birth exists -> if true, check if he's american
+            birth_place = data.get('place_of_birth')
+            places = ['United States', 'USA', 'U.S.']
+            is_american = any(place in birth_place for place in places) if birth_place else False
+            is_actor = data['known_for_department'] == "Acting"
 
-        if is_actor and is_american:
-            movie_info = get_movie_char(curr_id)
-            ret_list.append({"name": data['name'],
-                             "id": curr_id,
-                             "place_of_birth": birth_place,
-                             "movie_info": movie_info})
-        else:
-            print(f"is actor = {is_actor}, birth = {birth_place}")
-    return ret_list
+            if is_actor and is_american:
+                movie_info = get_movie_char(curr_id)
+                actor = {"name": data['name'],
+                                 "id": curr_id,
+                                 "place_of_birth": birth_place,
+                                 "movie_info": movie_info}
+                json_obj = json.dumps(actor, indent=4)
+                f.write(json_obj)
+            else:
+                print(f"is actor = {is_actor}, birth = {birth_place}")
 
 
 def extract_word_count(name_of_char, path_to_script):
@@ -96,19 +98,13 @@ def calc_popularity(mapping):
 
 
 if __name__ == '__main__':
-    #get_actors(6193)
-    # popular_actors = get_popular(3)
-    # mapping = get_birth_place(popular_actors)
-    actors = get_actors(200)
-    with open("./dataset.json", "w") as f:
-        json_obj = json.dumps(actors, indent=4)
-        f.write(json_obj)
+    create_database(100)
     # popular_actors = get_popular(2)
     # mapping = get_birth_place(popular_actors)
     # test = [{'name': 'Brad Pitt', 'id': 287,'place_of_birth': 'Shawnee, Oklahoma, USA',
     #         'movie_info':[{'title': 'Troy', 'character':'Achilles','rating': 7.1, 'vote_count':8514},
     #                       {'title': '12 Years a Slave', 'character':'Samuel Bass','rating': 8.0, 'vote_count':9705},
     #                       {'title': 'Fight Club','character':'Tyler Durden','rating': 8.4, 'vote_count':24204}]}]
-    test = dataset_to_list(r'/Users/iBenatar/PycharmProjects/HScProject/dataset.json')
-    pop_mapping = calc_popularity(test)
-    print("yo")
+    # test = dataset_to_list(r'/Users/iBenatar/PycharmProjects/HScProject/dataset.json')
+    # pop_mapping = calc_popularity(test)
+    # print("yo")
