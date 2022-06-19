aa3802b7a1a022b01f017f755504894b89f339a1
Arnon Portal <arnon.portal@gmail.com>
2022-06-19 15:55:15 +0300

    create database func

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def dataset_to_list(path_to_dataset):
         return json.load(file)
 
 
-
 def get_url_data(url):
     request = urllib.request.Request(url)
     result = urllib.request.urlopen(request)
@@ -23,9 +22,15 @@ def get_url_data(url):
 
 
 def get_movie_char(actor_id):
-    url = f"https://api.themoviedb.org/3/person/{actor_id}/" \
-          f"movie_credits?api_key=5f0c4de627a9354bdfcac23fbaacc3d2&language=en-US"
-    data = get_url_data(url)
+    try:
+        url = f"https://api.themoviedb.org/3/person/{actor_id}/" \
+              f"movie_credits?api_key=5f0c4de627a9354bdfcac23fbaacc3d2&language=en-US"
+        data = get_url_data(url)
+    except HTTPError as err:
+        if err.code != 200:
+            print(f"{err} for movie_char of id {curr_id}, skipping...")
+            return []
+
     ret_list = []
     for curr_movie in data['cast']:
         ret_list.append({"title": curr_movie['title'],
@@ -37,33 +42,43 @@ def get_movie_char(actor_id):
 
 
 def create_database(max_id):
-    ret_list = []
-    with open("./dataset.json", "w") as f:
-        for curr_id in range(1, max_id):
-            url = f"https://api.themoviedb.org/3/person/{curr_id}?api_key=5f0c4de627a9354bdfcac23fbaacc3d2&language=en-US"
-            try:
-                data = get_url_data(url)
-            except HTTPError as err:
-                if err.code != 200:
-                    print(f"Error {err} for id {curr_id}, skipping...")
-                    continue
+    data = None
 
-            # Check if place of birth exists -> if true, check if he's american
-            birth_place = data.get('place_of_birth')
-            places = ['United States', 'USA', 'U.S.']
-            is_american = any(place in birth_place for place in places) if birth_place else False
-            is_actor = data['known_for_department'] == "Acting"
+    for curr_id in range(1, max_id):
+        url = f"https://api.themoviedb.org/3/person/{curr_id}?api_key=5f0c4de627a9354bdfcac23fbaacc3d2&language=en-US"
+        try:
+            data = get_url_data(url)
+        except HTTPError as err:
+            if err.code != 200:
+                print(f"Error {err} for id {curr_id}, skipping...")
+                continue
 
-            if is_actor and is_american:
-                movie_info = get_movie_char(curr_id)
-                actor = {"name": data['name'],
-                                 "id": curr_id,
-                                 "place_of_birth": birth_place,
-                                 "movie_info": movie_info}
-                json_obj = json.dumps(actor, indent=4)
-                f.write(json_obj)
-            else:
-                print(f"is actor = {is_actor}, birth = {birth_place}")
+        # Check if place of birth exists -> if true, check if he's american
+        birth_place = data.get('place_of_birth')
+        places = ['United States', 'USA', 'U.S.']
+        is_american = any(place in birth_place for place in places) if birth_place else False
+        is_actor = data['known_for_department'] == "Acting"
+
+        if is_actor and is_american:
+            movie_info = get_movie_char(curr_id)
+            actor = {"name": data['name'],
+                             "id": curr_id,
+                             "place_of_birth": birth_place,
+                             "movie_info": movie_info}
+            # new_data = json.stringify(actor)
+            write_json(actor)
+
+
+def write_json(new_data, filename='dataset.json'):
+    with open(filename, 'r+') as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside emp_details
+        file_data.append(new_data)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent=4)
 
 
 def extract_word_count(name_of_char, path_to_script):
@@ -75,6 +90,7 @@ def extract_word_count(name_of_char, path_to_script):
             set_of_names.add(line[:line.find("=")])
         print("yo")
 
+
 def calc_popularity(mapping):
     path_to_scripts = r'/Users/iBenatar/PycharmProjects/HScProject/scripts/parsed'
     path_to_dialogues = os.path.join(path_to_scripts,'dialogue')
@@ -83,7 +99,7 @@ def calc_popularity(mapping):
         exit(1)
     suffix = "_dialogue.txt"
     for actor in mapping:
-        counter_found = 0;
+        counter_found = 0
         counter_not_found = 0
         for movie in actor['movie_info']:
             path_to_movie = os.path.join(path_to_dialogues,movie['title'].replace(" ","-").replace(":","")) + suffix
@@ -97,8 +113,17 @@ def calc_popularity(mapping):
         print(f"TOTAL SCRIPTS FOR {actor['name']}: FOUND - {counter_found}, NOT FOUND - {counter_not_found}")
 
 
+def create_empty_json():
+    f = open("./dataset.json", "w")
+    json.dump([], f, indent=4)
+    f.close()
+
+
+
 if __name__ == '__main__':
-    create_database(100)
+    create_empty_json()
+    create_database(40000)
+
     # popular_actors = get_popular(2)
     # mapping = get_birth_place(popular_actors)
     # test = [{'name': 'Brad Pitt', 'id': 287,'place_of_birth': 'Shawnee, Oklahoma, USA',
